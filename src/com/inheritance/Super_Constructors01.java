ec850707b0ca0bc666ec4c346ab0dc3dda596d9c
MPAVANKALYAN9515 <pavankalyan1691@gmail.com>
2025-02-24 00:54:16 +0530

    init commit Super_Constructors01

--- src/com/inheritance/Super_Constructors01.java
+++ src/com/inheritance/Super_Constructors01.java
@@ -1,23 +1,23 @@
 package com.inheritance;
 //MAHADASA CHANDU SIVA SANKARA PAVA KALYAN.
-class A1 {
-    public A1() {
+class A11 {
+    public A11() {
         System.out.println("Chef cooked PASTA");
     }
 }
-class A2 extends A1 {
-    public A2() {
+class A22 extends A11 {
+    public A22() {
         System.out.println("Student Added Cheese");
     }
 }
-class A3 extends A2 {
-    public A3() {
+class A33 extends A22 {
+    public A33() {
         System.out.println("Customer eats PASTA");
     }
 }
 public class Super_Constructors01 {
     public static void main(String[] args) {
-        A3 obj = new A3();
+        A33 obj = new A33();
     }
 }
 
